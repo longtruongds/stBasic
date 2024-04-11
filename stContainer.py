34b3cbff9a4edff4d4c258ecbbc92702bf7fa0fc
longtruongds <86976591+longtruongds@users.noreply.github.com>
2024-04-11 23:57:06 +0700

    Add files via upload

---
+++ stContainer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+
+# Insert elements using "with" notation
+with st.container():
+    st.write("This is inside the container")
+    st.bar_chart(np.random.randn(50, 3))  # You can call any Streamlit command
+
+# Elements can also be inserted out of order
+container = st.container()
+container.write("This is inside the container")
+st.write("This is outside the container")
+
+# Now insert some more elements in the container
+container.write("This is inside too")
+
